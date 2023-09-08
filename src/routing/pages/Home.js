eaba34ea4da109b0860a36d5dbc57c21a0476aa5
Oleksii Maslak <maslak4it@gmail.com>
2023-09-08 11:45:02 +0300

    comment section has been added, improved fetching service

--- src/routing/pages/Home.js
+++ src/routing/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1>Hello! Pls enjoy this funny video and go ahed with post page</h1>
+    <h1>Hello! Pls go ahed with post page and enjoy:)</h1>
   )
 }
 
--- src/routing/pages/PostIdPage.js
+++ src/routing/pages/PostIdPage.js
@@ -1,10 +1,58 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {useFetching} from '../../hooks/useFetching';
+import PostService from '../../API/PostService';
+import { useState } from 'react';
+import Loader from '../../components/UI/Loader/Loader'
+
 
 const PostIdPage = () => {
+  const params = useParams();
+  const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
+
+  const [fetchPostByID, isLoading, error] = useFetching ( async (id) => {
+    const response = await PostService.getById(id);
+    setPost(response.data)
+  })
+
+  const [fetchComments, isComLoading, comError] = useFetching ( async (id) => {
+    const response = await PostService.getCommentsByPostId(id);
+    console.log(response.data);
+    setComments(response.data)
+  })
+
+  useEffect(() => {
+    fetchPostByID(params.id);
+    fetchComments(params.id);
+
+
+  } , [])
+
   return (
-    <h1>
-        Post item was opened 
-    </h1>
+    <>
+      <h1>
+          Post item with id = {params.id} was opened. 
+      </h1>
+      {isLoading ? <Loader/> : <div>{post.id} {post.title}</div>}
+
+      <h1 style={{marginTop: 20}}>Comments:</h1>
+     
+      {isComLoading ? <Loader/> :     
+          <div>
+            {comments.map(comm => {
+              
+              return (
+                <div style={{marginTop: 15}}>
+                  <h4>{comm.email}</h4>
+                  <div>{comm.body}</div>
+                </div>
+              )
+            })}
+        </div>
+      }
+    </>  
   )
 }
 
